8ae3578e9d4ff5ebcfa78a93a4ce9ed16d312c3b
Matt Fritz <mfritz@itriagehealth.com>
2014-03-07 13:25:37 -0700

    add better error handling and messaging

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -10,8 +10,18 @@ module Rulers
 
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'}, [text]]
+
+      begin
+        text = controller.send(act)
+        response = 200
+      rescue Exception => e
+        text = "<h1>THERE WAS AN ERROR, DO NOT PANIC</h1>"
+        text << "<h2>Message</h2><p>#{e.message}</p>"
+        text << "<h2>Backtrace</h2><p>#{e.backtrace.inspect}</p>"
+        response = 500
+      end
+
+      [response, {'Content-Type' => 'text/html'}, [text]]
     end
   end
 
