3d80fdd561398f54dbc26cba94e899afb236bacf
Matt Fritz <mfritz@itriagehealth.com>
2014-03-12 12:41:23 -0600

    refactor controller class into controller.rb

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,6 +2,7 @@ require "rulers/version"
 require "rulers/routing"
 require "rulers/util"
 require "rulers/dependencies"
+require "rulers/controller"
 
 module Rulers
   class Application
@@ -35,16 +36,6 @@ module Rulers
       end
 
       [response, {'Content-Type' => 'text/html'}, [text]]
-    end
-  end
-
-  class Controller
-    def initialize(env)
-      @env = env
-    end
-
-    def env
-      @env
     end
   end
 end
